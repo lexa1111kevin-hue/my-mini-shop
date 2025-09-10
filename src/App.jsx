8047f4401baa0f21f98272ffbf3a397efe4122cc
lexa1111kevin-hue <lexa1111kevin@gmail.com>
2025-09-10 19:05:11 +0500

    Удалил node_modules и dist из репозитория, добавил .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import CompanyHeader from "./components/CompanyHeader";
 import Header from "./components/header";
